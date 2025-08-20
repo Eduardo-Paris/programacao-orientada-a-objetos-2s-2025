690ed43c6dec732f5c4ecad9c4c2a660637c7a37
Eduardo-Paris <edubp2512@gmail.com>
2025-08-20 12:24:54 -0300

    commitamazing

---
+++ ExecutaCauculadora.java
@@ -0,0 +1,21 @@
+import cauculadora.Cauculadora;
+
+
+public class ExecutaCauculadora {
+    
+public static void main(String[] args){
+Calculadora calc1 = new Calculadora ();
+Calculadora calc2 = new Calculadora ();
+
+calc1.numeroA = 2.0;
+calc1.numeroB = 3.0;
+
+calc2.numeroA = 10.0;
+calc2.numeroB = 6.0;
+
+calc1. somar();
+calc2. somar();
+
+}
+
+}
---
+++ cauculadora/Cauculadora.java
@@ -0,0 +1,16 @@
+package cauculadora;
+
+public class Cauculadora {
+    
+    public Double numeroA;
+    public Double numeroB;
+
+    public void somar(){
+        //2+3=x
+        Double resultadoSoma = this.numeroA + this.numeroB;
+    System.out.println(numeroA + " + " + this.numeroB + " = " + resultadoSoma);
+    }
+
+
+
+}
